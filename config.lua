ce1589781919fcc6e2b6fa7431931ddcc8dc4f13
abolfazlabbasifx <mycarphone001@gmail.com>
2017-04-13 19:54:55 +0430

    jsjxjx

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-bot_token = ""
+bot_token = "377353292:AAFLustMRvt2bs3v51DDUekuLIdxr2hCafI"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "2.5"
-sudo_id = 212400472
+sudo_id = 309730638
